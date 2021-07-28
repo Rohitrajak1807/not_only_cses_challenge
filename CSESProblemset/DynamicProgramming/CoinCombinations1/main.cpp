11c711537ab77c7f4d0ed22e638adb1958998ec1
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-07-28 17:13:57 +0530

    coin combination 1 simplify conditional change initial values of table to 0

--- CSESProblemset/DynamicProgramming/CoinCombinations1/main.cpp
+++ CSESProblemset/DynamicProgramming/CoinCombinations1/main.cpp
@@ -16,17 +16,13 @@ int main() {
 }
 
 int32_t coin_combinations(int32_t target, const std::vector<int32_t> &coins) {
-	std::vector<int32_t> table(target + 1, -1);
+	std::vector<int32_t> table(target + 1, 0);
 	table[0] = 1;
 	for (int32_t i = 0; i <= target; ++i) {
 		if (table[i] != -1) {
 			for (const int32_t &coin : coins) {
 				if (i + coin <= target) {
-					if (table[i + coin] == -1) {
-						table[i + coin] = table[i];
-					} else {
-						table[i + coin] = ((table[i + coin] % MODULUS) + (table[i] % MODULUS)) % MODULUS;
-					}
+					table[i + coin] = ((table[i + coin] % MODULUS) + (table[i] % MODULUS)) % MODULUS;
 				}
 			}
 		}
