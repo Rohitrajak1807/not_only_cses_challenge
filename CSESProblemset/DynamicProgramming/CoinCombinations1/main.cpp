0c62c97abc97e58ae790a8231b4277b7a11055be
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-07-28 17:11:17 +0530

    add coin combination 1

---
+++ CSESProblemset/DynamicProgramming/CoinCombinations1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+#define MODULUS 1000000007
+
+int32_t coin_combinations(int32_t target, const std::vector<int32_t> &coins);
+
+int main() {
+	int32_t length, target;
+	std::cin >> length >> target;
+	std::vector<int32_t> coins(length);
+	for (int32_t &i : coins)
+		std::cin >> i;
+	std::cout << coin_combinations(target, coins) << '\n';
+	return 0;
+}
+
+int32_t coin_combinations(int32_t target, const std::vector<int32_t> &coins) {
+	std::vector<int32_t> table(target + 1, -1);
+	table[0] = 1;
+	for (int32_t i = 0; i <= target; ++i) {
+		if (table[i] != -1) {
+			for (const int32_t &coin : coins) {
+				if (i + coin <= target) {
+					if (table[i + coin] == -1) {
+						table[i + coin] = table[i];
+					} else {
+						table[i + coin] = ((table[i + coin] % MODULUS) + (table[i] % MODULUS)) % MODULUS;
+					}
+				}
+			}
+		}
+	}
+	return table[target] == -1 ? 0 : table[target];
+}
