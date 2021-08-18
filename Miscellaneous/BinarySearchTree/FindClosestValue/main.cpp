cdc6c75eef50ae49152b59683ac1126081089e8d
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-08-18 17:42:38 +0530

    update find_closest_value in a bst - add problem statement

--- Miscellaneous/BinarySearchTree/FindClosestValue/main.cpp
+++ Miscellaneous/BinarySearchTree/FindClosestValue/main.cpp
@@ -2,6 +2,31 @@
 #include <array>
 #include <iostream>
 
+
+// Write a function that takes in a Binary Search Tree (BST) and a target integer
+// value and returns the closest value to that target value contained in the BST.
+// You can assume that there will only be one closest value.
+
+// Each BST node has an integer , a left child node, and a right child node.
+// A node is said to be a valid BST node if and only if it satisfies the BST property:
+// its value is strictly greater than the values of every node to its left;
+// its value is less than or equal to the values of every node to its right;
+// and its children nodes are either valid BST nodes themselves or None / null.
+// Sample Input
+// tree =
+//	  		    10
+//           /     \
+//          5      15
+//        /   \   /   \
+//       2     5 13   22
+//      /           \
+//     1            14
+//
+// target = 13
+// Sample Output
+// 13
+
+
 int32_t closest_value(Node *root, int32_t target);
 
 int32_t get_closest_val(Node *root, int32_t target, int32_t closest);
