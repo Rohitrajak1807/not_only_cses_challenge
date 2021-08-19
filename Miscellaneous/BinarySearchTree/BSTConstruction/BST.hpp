d358913a33a1791d7d7e07b27cd50c17395503a4
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-08-19 14:51:25 +0530

    bst done - lesson learnt: never forget the basics of c++ templates(template specialization are generated during compilation)

--- Miscellaneous/BinarySearchTree/BSTConstruction/BST.hpp
+++ Miscellaneous/BinarySearchTree/BSTConstruction/BST.hpp
@@ -2,7 +2,49 @@
 // Created on 8/19/21.
 //
 
-#include "BST.h"
+#ifndef BSTCONSTRUCTION_BST_HPP
+#define BSTCONSTRUCTION_BST_HPP
+#include <memory>
+#include <vector>
+
+template<typename T>
+struct Node {
+	T value;
+	std::unique_ptr <Node> left;
+	std::unique_ptr <Node> right;
+
+	explicit Node(T &val);
+};
+
+template<typename T>
+class BST {
+public:
+	BST();
+
+	explicit BST(T val);
+
+	explicit BST(std::vector <T> &arr);
+
+	void insert(T &value);
+
+	void insert(T &&value);
+
+	bool contains(const T &target);
+
+	bool remove_val(const T &target);
+
+private:
+	std::unique_ptr <Node<T>> root;
+
+	void insert(T &value, std::unique_ptr <Node<T>> &current_node);
+
+	bool del(const T &target, std::unique_ptr <Node<T>> &current_node);
+
+	void delete_node(std::unique_ptr <Node<T>> &current_node);
+
+	T get_successor_value(std::unique_ptr <Node<T>> &current_node);
+};
+
 
 template<typename T>
 Node<T>::Node(T &val) : value(val) {}
@@ -89,3 +131,5 @@ T BST<T>::get_successor_value(std::unique_ptr<Node<T>> &current_node) {
 	}
 	return get_successor_value(current_node->left);
 }
+
+#endif //BSTCONSTRUCTION_BST_HPP
--- Miscellaneous/BinarySearchTree/BSTConstruction/main.cpp
+++ Miscellaneous/BinarySearchTree/BSTConstruction/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "BST.h"
+#include "BST.hpp"
 #include <vector>
 
 //template<typename T>
