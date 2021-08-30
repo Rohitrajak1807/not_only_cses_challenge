96c644fb8705fac209062e5f8d94bd35713cfb70
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-08-30 20:56:10 +0530

    update max subset sum no adjacent make solution take constant space add problem statement

--- Miscellaneous/DynamicProgramming/MaxSubsetSumNoAdjacent/main.cpp
+++ Miscellaneous/DynamicProgramming/MaxSubsetSumNoAdjacent/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 
+
+// Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
+// If the input array is empty, the function should return 0.
+// Sample Input
+// array  = [75, 105, 120, 75, 90, 135]
+//
+// Sample Output
+// 330
+
 int32_t max_sum_no_adj(const std::vector<int32_t> &v);
 
 int main() {
@@ -12,11 +21,12 @@ int main() {
 int32_t max_sum_no_adj(const std::vector<int32_t> &v) {
 	if (v.empty())
 		return 0;
-	std::vector<int32_t> max_sum(v.size());
-	max_sum[0] = v[0];
-	max_sum[1] = std::max(v[0], v[1]);
-	for (size_t i = 2; i < v.size(); ++i)
-		max_sum[i] = std::max(max_sum[i - 2] + v[i], max_sum[i - 1]);
 
-	return max_sum.back();
+	int32_t s0 = v[0], s1 = std::max(v[0], v[1]), s = std::max(s0, s1);
+	for (size_t i = 2; i < v.size(); ++i) {
+		s = std::max(s0 + v[i], s1);
+		s0 = s1;
+		s1 = s;
+	}
+	return s;
 }
