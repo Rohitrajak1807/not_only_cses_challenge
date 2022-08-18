2165e04153a7fefd7949d1241f5b6c5508cbadf3
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2022-08-18 23:00:49 +0530

    add strings/tokenization implement a simple tokenizer

--- Miscellaneous/Strings/Tokenisation/main.cpp
+++ Miscellaneous/Strings/Tokenisation/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cstring>
 
 std::vector<std::string> using_sstream(const std::string& in);
+
+std::string tokenize(char *str, char delim);
 
 int main() {
 	std::string in;
@@ -10,6 +13,13 @@ int main() {
 	auto tokens = using_sstream(in);
 	for (std::string& a: tokens) {
 		std::cout << a << '\n';
+	}
+	char s[1000];
+	std::cin.getline(s, 1000);
+	std::string token = tokenize(s, ' ');
+	while (!token.empty()) {
+		std::cout << token << '\n';
+		token = tokenize(nullptr, ' ');
 	}
 	return 0;
 }
@@ -23,4 +33,26 @@ std::vector<std::string> using_sstream(const std::string& in) {
 		tokens.emplace_back(tok);
 	}
 	return tokens;
+}
+
+std::string tokenize(char *str, char delim) {
+	static char *input = nullptr;
+	if (str != nullptr) {
+		input = str;
+	}
+	if (input == nullptr) {
+		return "";
+	}
+	std::string token;
+	int32_t i = 0;
+	for(;input[i] != '\0'; ++i) {
+		if (input[i] != delim) {
+			token.push_back(input[i]);
+		} else {
+			input = input + i + 1;
+			return token;
+		}
+	}
+	input = nullptr;
+	return token;
 }
