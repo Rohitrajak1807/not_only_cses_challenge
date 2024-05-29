81030453f9703d993c3af0fafa9ade1fb9e359e1
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2024-05-29 23:32:44 +0530

    tiling dominoes and trominoes, iterative wip

--- Miscellaneous/DynamicProgramming/DominoesAndTrominoes/main.go
+++ Miscellaneous/DynamicProgramming/DominoesAndTrominoes/main.go
@@ -24,6 +24,14 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(tileRecurse(num))
+	fmt.Println(tileIterative(num))
+	for i := range 1000 {
+		val1 := tileRecurse(i)
+		val2 := tileIterative(i)
+		if val1 != val2 {
+			panic(fmt.Sprintf("expected %d got %d iteration %d", val1, val2, i))
+		}
+	}
 }
 
 /*
@@ -36,7 +44,6 @@ dp[i][3] += dp[i - 1][3] + dp[i - 1][0]
 
 ^ Hold this thought for the iterative solution
 
-
 |-----|------|------|------|
 |     |      |   t1 |  t3  |
 |-----|------|------|------|
@@ -44,13 +51,11 @@ dp[i][3] += dp[i - 1][3] + dp[i - 1][0]
 |-----|------|------|------|
 
 t1,t2,t3 and t4 represent the cells which can be filled in the ith iteration
-
-
 */
-
 func tileRecurse(num int) int {
 	dp := make([][4]int, num+1)
-	return _tileRecurse(0, num, true, true, dp)
+	val := _tileRecurse(0, num, true, true, dp)
+	return val
 }
 
 func _tileRecurse(i, num int, isT1Free, isT2Free bool, dp [][4]int) int {
@@ -105,10 +110,34 @@ func makeState(t1, t2 bool) int {
 	return state // if both t1 and t2 are true then state becomes the 11 state
 }
 
-//func tile(num int64) int64 {
-//	dp := make([][4]int64, num+1)
-//
-//	for i := 2; i < len(dp); i++ {
-//
-//	}
-//}
+/*
+there are 4 possible states to keep track of:
+00,01,10,11
+dp[i][0] += dp[i - 1][3]
+dp[i][1] += dp[i - 1][2] + 1
+dp[i][2] += dp[i - 1][1] + 1
+dp[i][3] += dp[i - 1][3] + dp[i - 1][0]
+
+|-----|------|------|------|
+|     |      |   t1 |  t3  |
+|-----|------|------|------|
+|     |      |   t2 |  t4  |
+|-----|------|------|------|
+
+t1,t2,t3 and t4 represent the cells which can be filled in the ith iteration
+
+The solution isn't complete, iterative solution seems to calculate a higher number, the first occurrence of this is at
+i = 28
+*/
+func tileIterative(num int) int {
+	dp := make([][4]int, num+1)
+	dp[0][3] = 1
+	for i := 1; i < len(dp); i++ {
+		dp[i][0] += dp[i-1][3] % modulo
+		dp[i][1] += (dp[i-1][2] % modulo) + (dp[i-1][0] % modulo)
+		dp[i][2] += (dp[i-1][1] % modulo) + (dp[i-1][0] % modulo)
+		dp[i][3] += (dp[i-1][3] % modulo) + (dp[i-1][0] % modulo) + (dp[i-1][1] % modulo) + (dp[i-1][2] % modulo)
+
+	}
+	return dp[num][3]
+}
